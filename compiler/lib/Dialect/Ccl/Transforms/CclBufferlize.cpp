e7738cd186edf70e7edb387eeeabb1ac8c85de05
root <root@n141-003-168.byted.org>
2024-02-26 14:05:49 +0000

    add bufferlize.

--- compiler/lib/Dialect/Ccl/Transforms/CclBufferlize.cpp
+++ compiler/lib/Dialect/Ccl/Transforms/CclBufferlize.cpp
@@ -3,6 +3,7 @@
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Dialect/Bufferization/IR/BufferizableOpInterface.h"
 #include "mlir/Dialect/Bufferization/IR/Bufferization.h"
+#include "mlir/Dialect/Bufferization/Transforms/Bufferize.h"
 #include "PassDetail.h"
 
 #define DEBUG_TYPE "ccl-move-down"
@@ -28,10 +29,29 @@ struct BroadcastOpInterface : public BufferizableOpInterface::ExternalModel<Broa
   }
 
   AliasingValueList getAliasingValues(Operation *op, OpOperand &, const AnalysisState &) const {
-    return {{op->getResult(0), BufferRelation::Equivalent}};
+    return {};
   }
 
   LogicalResult bufferize(Operation *op, RewriterBase &rewriter, const BufferizationOptions &options) const {
+    auto broadcastOp = cast<ccl::BroadcastOp>(op);
+    FailureOr<Value> srcBuffer = getBuffer(rewriter, broadcastOp.getSrc(), options);
+    FailureOr<Value> dynamicReplicaGroupsBuffer = Value();
+    if (broadcastOp.getDynamicReplicaGroups().getImpl())
+      dynamicReplicaGroupsBuffer = getBuffer(rewriter, broadcastOp.getDynamicReplicaGroups(), options);
+    if (failed(srcBuffer))
+      return failure();
+    Type resultType;
+    auto opResultType = broadcastOp.getType(0);
+    if (auto rankedType = opResultType.dyn_cast<RankedTensorType>()) {
+      resultType = MemRefType::get(rankedType.getShape(), rankedType.getElementType());
+    } else if (auto unrankedType = opResultType.dyn_cast<UnrankedTensorType>()) {
+      resultType = mlir::UnrankedMemRefType::get(unrankedType.getElementType(), 0);
+    }
+    broadcastOp = bufferization::replaceOpWithNewBufferizedOp<ccl::BroadcastOp>(rewriter, op, resultType, srcBuffer.value(), dynamicReplicaGroupsBuffer.value(), broadcastOp.getSynchronous(), broadcastOp.getReplicaGroupsAttr(), broadcastOp.getUniqueIdAttr());
+    rewriter.replaceAllUsesWith(broadcastOp.getResult(), srcBuffer.value());
+    rewriter.setInsertionPoint(broadcastOp);
+    rewriter.create<ccl::BroadcastOp>(broadcastOp.getLoc(), TypeRange(), srcBuffer.value(), dynamicReplicaGroupsBuffer.value(), broadcastOp.getSynchronous(), broadcastOp.getReplicaGroupsAttr(), broadcastOp.getUniqueIdAttr());
+    rewriter.eraseOp(broadcastOp);
     return success();
   }
 };
@@ -39,13 +59,17 @@ struct BroadcastOpInterface : public BufferizableOpInterface::ExternalModel<Broa
 struct CclBufferlizePass : public CclBufferlizeBase<CclBufferlizePass> {
   CclBufferlizePass() : CclBufferlizeBase() {}
   void runOnOperation() override {
-
+    BufferizationOptions options = bufferization::getPartialBufferizationOptions();
+    options.opFilter.allowDialect<ccl::CclDialect>();
+    if (failed(bufferizeOp(getOperation(), options))) 
+      signalPassFailure();
   }
 
   void getDependentDialects(DialectRegistry &registry) const override {
     registry.insert<ccl::CclDialect>();
     registry.addExtension(+[](MLIRContext *ctx, ccl::CclDialect * /*dialect*/) {
-    ccl::BroadcastOp::attachInterface<BranchOpInterface>(*ctx);
+    ccl::BroadcastOp::attachInterface<BroadcastOpInterface>(*ctx);
+    ctx->loadDialect<bufferization::BufferizationDialect>();
   });
   }
 };
